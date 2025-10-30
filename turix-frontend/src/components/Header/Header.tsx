7cd81e9396caf81362baaca967a2c42ddf23fcd6
Laedson <laedson2535@gmail.com>
2025-10-30 14:58:49 -0300

    Adicionando Header e Footer

---
+++ turix-frontend/src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { headers } from "next/headers";
+import  styles  from "./Header.module.css"
+import Link from "next/link";
+
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.logo}>Turix</h1>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/">Destinos</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Conta</Link>
+            </nav>
+
+        </header>
+    );
+}
